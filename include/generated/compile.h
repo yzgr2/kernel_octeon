fa59445afb002f35568fbfccf6d06b39e6c642b8
yzgr2 <279785882@qq.com>
2016-07-06 15:18:01 +0800

    include folder add

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP */
+#define UTS_MACHINE "mips64"
+#define UTS_VERSION "#2 SMP Thu Feb 27 01:19:24 HKT 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "cc"
+#define LINUX_COMPILER "gcc version 4.7.0 (Cavium Inc. Version: SDK_3_1_0 build 32) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.10.20-rt14-Cavium-Octeon"
